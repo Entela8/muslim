80c6a058e29194636632c483105a5e17afa1b6f3
Entela Mema <entela599@gmail.com>
2025-02-22 15:44:58 +0100

    removed firebasse

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,6 @@ import 'package:muslim_alim/screens/webview_screen.dart';
 import 'package:muslim_alim/services/local_notification_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:firebase_core/firebase_core.dart';
-import 'package:muslim_alim/services/push_notification_service.dart';
-import 'firebase_options.dart';
 
 void main() async {
   // initialization
@@ -16,15 +13,9 @@ void main() async {
   // Update widget location
   prefs.setBool('shouldUpdateWidgetLocation', true);
 
-  // Initialize Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   tz.initializeTimeZones();
 
   await LocalNotification.initialize();
-  await PushNotificationService.initialize();
 
   // Schedule prayer notifications
   prefs.setBool('shouldSchedulePrayerNotifications', true);
